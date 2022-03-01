91c45b2f9c624fff6412a875b71965a9c1d872bf
alexisR <alexisrojasf@outlook.es>
2022-03-01 08:59:57 -0500

    first commit

---
+++ src/main/java/com/example/service/CityService.java
@@ -0,0 +1,39 @@
+package com.example.service;
+
+import com.example.entity.City;
+import com.example.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    public void save(City city){
+        cityRepository.save(city);
+    }
+
+    public List<City> getAll() throws SQLException {
+        return cityRepository.findAll();
+
+
+    }
+
+    public Optional<City> get(Long id){
+        return cityRepository.findById(id);
+    }
+
+    public City getByName(String name){
+        return cityRepository.findByName(name);
+    }
+
+    public void delete(String name){
+        cityRepository.deleteByName(name);
+    }
+}
